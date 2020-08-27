ccf1488c526e4c540f4879c54ff8ac44d1371f53
Aaron Gee-Clough <spamblock@g-clef.net>
2020-08-27 10:43:13 -0400

    shift version to allow check in faust

--- tests/test_client.py
+++ tests/test_client.py
@@ -35,7 +35,7 @@ class TestAIOKafkaClient(unittest.TestCase):
             loop=self.loop, bootstrap_servers=[
                 '127.0.0.1:9092', '127.0.0.2:9092', '127.0.0.3:9092'])
         self.assertEqual(
-            '<AIOKafkaClient client_id=aiokafka-1.1.6>',
+            '<AIOKafkaClient client_id=aiokafka-1.1.7-dt>',
             client.__repr__())
         self.assertEqual(
             sorted([('127.0.0.1', 9092, socket.AF_INET),
